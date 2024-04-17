a17a8477844852deecec08864bb227b3fcb3b6a3
Yuxi Dai <yuxid5@uci.edu>
2024-04-17 15:28:11 -0700

    test streamlit

--- zot.py
+++ zot.py
@@ -1,7 +1,14 @@
-#start here
+import streamlit as st
+import pandas as pd
 
-
+st.title("Forum Chat_with_streamlit")
+st.write("Hello world")
+if st.button("click"):
+    st.write("send" * 3)
+if st.button("click2"):
+    st.write("shabi")
 
 
 if __name__ == "__main__":
     print(1)
+    print("hello world")
