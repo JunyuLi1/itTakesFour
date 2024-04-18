6460b5eb736136ad4f9c290337fe1d24afb94007
Junyu Li <junyul031030@gmail.com>
2024-04-18 16:53:43 -0700

    Update

--- zot.py
+++ zot.py
@@ -1,35 +1,40 @@
 import streamlit as st
-import socket
 import ds_client
 
 st.session_state.messages = []
-
-def create_friendlist():
-    st.sidebar.title("Friend Lists")
-    friend = st.sidebar.radio("", list(st.session_state.chat_logs.keys()))
-    message_input(friend)
-    st.write(f"Chat history with {friend}:")
-    for message in st.session_state.chat_logs[friend]:
-        st.text(message)
-
-
-def message_input(target_name):
-    with st.container():
-        message_input = st.text_input("Type your message here...", key="message_input")
-        if st.button("Send", key="send"):
-            if message_input: # not empty
-                st.session_state.chat_logs[target_name].append(message_input)
-                ds_client.send('168.235.86.101', 3021, 'VC1', 'VC', message=message_input)   
 
 
 def main():
     if 'chat_logs' not in st.session_state:
         st.session_state.chat_logs = {
-            "Alice": ["CS121 is hard"],
-            "Bob": ["Project2 is hard"],
-            "Charlie": ["I can't finish pj2"]
+            "Alan": ["How's your pj2?", "sounds good", "bye"],
+            "Frank": ["Hello World", "Nice to meet you"],
+            "Shelly": ["Test1", "Test2"]
         }
     create_friendlist()
+
+
+def create_friendlist():
+    st.sidebar.title("Friend Lists")
+    friend_names = list(st.session_state.chat_logs.keys())
+    friend = st.sidebar.radio("", friend_names)
+    display_chat_log(friend)
+    message_input(friend)
+
+def display_chat_log(friend):
+    st.write(f"Chat history with {friend}:")
+    for message in st.session_state.chat_logs[friend]:
+        st.text(message)
+
+def message_input(target_name):
+    with st.container():
+        message_input = st.text_input("Type your message here...", key="message_input")
+        if st.button("Send", key="send"):
+            if message_input:
+                st.session_state.chat_logs[target_name].append(message_input)
+                ds_client.send('168.235.86.101', 3021, 'VC1', 'VC', message=message_input)
+
+
 
 
 if __name__ == "__main__":
