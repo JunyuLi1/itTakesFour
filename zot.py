ac2fb5924a4d3d5c3848b99665035314a52bedc6
Yuxi Dai <yuxid5@uci.edu>
2024-04-18 16:51:46 -0700

    adjust plot

--- zot.py
+++ zot.py
@@ -2,24 +2,28 @@ import streamlit as st
 
 st.session_state.messages = []
 
-# st.session_state.chat_logs = {
-#         "Alice": ["你好，Alice！", "最近怎么样？", "听说你去旅行了！"],
-#         "Bob": ["嘿，Bob，周末打算做什么？", "我们去徒步如何？"],
-#         "Charlie": ["Charlie，下次聚会你准备带些什么？", "我可以带些饮料。"]
-#     }
+
+def main():
+    if 'chat_logs' not in st.session_state:
+        st.session_state.chat_logs = {
+            "Alan": ["How's your pj2?", "sounds good", "bye"],
+            "Frank": ["Hello World", "Nice to meet you"],
+            "Shelly": ["Test1", "Test2"]
+        }
+    create_friendlist()
 
 
 def create_friendlist():
     st.sidebar.title("Friend Lists")
-    friend_names = ["sbDawei","shabiFrank"]
-    friend = st.sidebar.radio("", list(st.session_state.chat_logs.keys()))
+    friend_names = list(st.session_state.chat_logs.keys())
+    friend = st.sidebar.radio("", friend_names)
+    display_chat_log(friend)
     message_input(friend)
+
+def display_chat_log(friend):
     st.write(f"与 {friend} 的聊天记录：")
     for message in st.session_state.chat_logs[friend]:
         st.text(message)
-
-def load_chat_history(friend):
-    pass
 
 def message_input(target_name):
     with st.container():
@@ -30,14 +34,6 @@ def message_input(target_name):
                 # st.session_state.messages.append(message_input)   
 
 
-def main():
-    if 'chat_logs' not in st.session_state:
-        st.session_state.chat_logs = {
-            "Alice": ["你好，Alice！", "最近怎么样？", "听说你去旅行了！"],
-            "Bob": ["嘿，Bob，周末打算做什么？", "我们去徒步如何？"],
-            "Charlie": ["Charlie，下次聚会你准备带些什么？", "我可以带些饮料。"]
-        }
-    create_friendlist()
 
 
 if __name__ == "__main__":
