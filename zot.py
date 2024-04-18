06131fe7aeed68cd2503661c7a6375c2b128a090
Junyu Li <junyul031030@gmail.com>
2024-04-18 16:49:46 -0700

    connect to the server and test

--- zot.py
+++ zot.py
@@ -1,29 +1,35 @@
 import streamlit as st
+import socket
+import ds_client
 
 st.session_state.messages = []
 
-def create_header():
+def create_friendlist():
     st.sidebar.title("Friend Lists")
-    friend_names = ["sbDawei","shabiFrank"]
-    for item in friend_names:
-        st.sidebar.button(item)
-
-
-with st.container():
-    st.write('shabidawei')
-    for message in st.session_state.messages:
+    friend = st.sidebar.radio("", list(st.session_state.chat_logs.keys()))
+    message_input(friend)
+    st.write(f"Chat history with {friend}:")
+    for message in st.session_state.chat_logs[friend]:
         st.text(message)
 
 
-with st.container():
-    message_input = st.text_input("Type your message here...", key="message_input")
-    if st.button("Send", key="send"):
-        if message_input:  # 确保消息不为空
-            st.session_state.messages.append(message_input)   
+def message_input(target_name):
+    with st.container():
+        message_input = st.text_input("Type your message here...", key="message_input")
+        if st.button("Send", key="send"):
+            if message_input: # not empty
+                st.session_state.chat_logs[target_name].append(message_input)
+                ds_client.send('168.235.86.101', 3021, 'VC1', 'VC', message=message_input)   
 
 
 def main():
-    create_header()
+    if 'chat_logs' not in st.session_state:
+        st.session_state.chat_logs = {
+            "Alice": ["CS121 is hard"],
+            "Bob": ["Project2 is hard"],
+            "Charlie": ["I can't finish pj2"]
+        }
+    create_friendlist()
 
 
 if __name__ == "__main__":
