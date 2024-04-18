035b27b9f78d9bc6136300e03df486b3e0359561
Junyu Li <junyul031030@gmail.com>
2024-04-17 17:07:09 -0700

    Add freind list and test send'

--- zot.py
+++ zot.py
@@ -1,8 +1,29 @@
 import streamlit as st
 
+st.session_state.messages = []
+
+def create_header():
+    st.sidebar.title("Friend Lists")
+    friend_names = ["sbDawei","shabiFrank"]
+    for item in friend_names:
+        st.sidebar.button(item)
+
+
+with st.container():
+    st.write('shabidawei')
+    for message in st.session_state.messages:
+        st.text(message)
+
+
+with st.container():
+    message_input = st.text_input("Type your message here...", key="message_input")
+    if st.button("Send", key="send"):
+        if message_input:  # 确保消息不为空
+            st.session_state.messages.append(message_input)   
+
 
 def main():
-    st.title('Streamlit 示例应用')
+    create_header()
 
 
 if __name__ == "__main__":
