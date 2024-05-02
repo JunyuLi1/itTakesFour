c0144d4e213e6a1e71ef3edc42a41de9c1400e3b
Junyu <junyul24>
2024-05-02 01:33:14 -0700

    first version

--- zot.py
+++ zot.py
@@ -1,8 +1,60 @@
 import streamlit as st
+import ds_client
+import ds_messenger
+
+st.session_state.messages = []
+st.session_state.friends = {}
+contactobj = ds_messenger.DirectMessenger('168.235.86.101','VC1', 'VC')
 
 
 def main():
-    st.title('Streamlit 示例应用')
+    if 'chat_logs' not in st.session_state:
+        st.session_state.chat_logs = {
+            "Alan": ["How's your pj2?", "sounds good", "bye"],
+            "Frank": ["Hello World", "Nice to meet you"],
+            "Shelly": ["Test1", "Test2"]
+        }
+    create_friendlist()
+
+
+
+
+def create_friendlist():
+    st.sidebar.title("Friend Lists")
+    friend_names = list(st.session_state.chat_logs.keys())
+    friend = st.sidebar.radio("", friend_names)
+    display_chat_log(friend)
+    message_input(friend)
+    add_new_contact()
+
+def display_chat_log(friend):
+    st.write(f"Chat history with {friend}:")
+    for message in st.session_state.chat_logs[friend]:
+        st.text(message)
+
+def message_input(target_name):
+    with st.container():
+        message_input = st.text_input("Type your message here...", key="message_input")
+        if st.button("Send", key="send"):
+            if message_input:
+                st.session_state.chat_logs[target_name].append(message_input)
+                print(contactobj.send(message_input,target_name))
+
+def add_new_contact():
+    with st.sidebar:
+        new_contact = st.text_input("Add new contact name", key="new_contact")
+        if st.button("Add Contact"):
+            if new_contact:
+                if new_contact not in st.session_state.friends:
+                    st.session_state.friends[new_contact] = []
+                    temp = []
+                    temp.append(new_contact)
+                    new_contact = temp
+                    st.sidebar.radio("", new_contact)
+                else:
+                    st.sidebar.error("Contact already exists.")
+
+
 
 
 if __name__ == "__main__":
