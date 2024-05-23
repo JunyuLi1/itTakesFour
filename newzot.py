3c63e99c20bf67853732f2183203658fb6af5ebd
JunyuLi1 <junyul031030@gmail.com>
2024-05-23 16:31:13 -0700

    split chat history

--- newzot.py
+++ newzot.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 import ds_messenger
 from message_style import Message
 import time
 
 
-#{'something': {'SuperHammerA': [<message_style.Message object at 0x000002223E785910>]}}
+#{'something': {'SuperHammerB': [<message_style.Message object at 0x0000020A5AC78250>], 
+#'Alan': [<message_style.Message object at 0x0000020A4C8B2610>], 'user': []}}
 class Chat():
     def __init__(self, ip, ucername, password):
         self.contactobj = ds_messenger.DirectMessenger(ip,ucername, password)
@@ -13,7 +15,7 @@ class Chat():
         if 'chat_logs' not in st.session_state:
             st.session_state.chat_logs = {
                 "Alan": [Message("How's your pj2?", 1), Message("sounds good", 2), Message("bye", 3)],
-                "SuperHammerA": [Message("Hello World", 1), Message("Nice to meet you", 2)],
+                "SuperHammerB": [],
                 "SuperHammerD": [Message("Test1", 1), Message("Test2", 2)]
             }
             st.session_state.group_chat = {}
@@ -34,31 +36,40 @@ class Chat():
 
 
     def display_chat_log(self, friend):
-        st.write(f"Chat history with {friend}:")
-        display_log = st.empty()
-        for message in st.session_state.chat_logs[friend]:
-            st.text(message.get_message())
-        result = self.contactobj.retrieve_all()
-        for item in result:
-            if str(friend) == str(item.recipient):
-                temp_mes = Message(item.message, item.timestamp)
-                if (temp_mes in st.session_state.chat_logs[friend]):
-                    pass
-                else:
-                    st.text(temp_mes.message)
-                    st.session_state.chat_logs[friend].append(temp_mes)
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write(f"Chat history with {friend}:")
+            for message in st.session_state.chat_logs[friend]:
+                st.text(message.get_message())
+            result = self.contactobj.retrieve_all()
+            for item in result:
+                if str(friend) == str(item.recipient):
+                    temp_mes = Message(item.message, item.timestamp)
+                    if (temp_mes in st.session_state.chat_logs[friend]):
+                        pass
+                    else:
+                        st.text(temp_mes.message)
+                        st.session_state.chat_logs[friend].append(temp_mes)
+        with right_column:
+            st.write('test')
 
 
     def display_groupchat_log(self, friend):
-        st.write(f"Group history of {friend}:")
+        left_column, right_column = st.columns(2)
         contect_dic = st.session_state.group_chat[friend]
-        #receive message
-        for item in contect_dic:
-            if len(contect_dic[item])>=1:
-                st.write(f'{item} said:')
-                for itme2 in contect_dic[item]:
-                    st.text(itme2.get_message())
-
+        with left_column:
+            st.write(f"Group history of {friend}:")
+            #receive message
+            for item in contect_dic:
+                if len(contect_dic[item])>=1:
+                    if str(item) != str('user'):
+                        st.write(f'{item} said:')
+                        for itme2 in contect_dic[item]:
+                            st.text(itme2.get_message())
+        with right_column:
+            #receive message
+            for item in contect_dic['user']:
+                st.text(item.get_message())
 
     def message_input(self, target_name):
         with st.container():
@@ -109,12 +120,12 @@ class Chat():
                     st.session_state.chat_logs[group_name] = {}
                     for item in selected_contacts:
                         st.session_state.group_chat[group_name][item] = [Message(f'Hi from {item}',1)]
-                    st.session_state.group_chat[group_name]['user'] = []
-                    print(st.session_state.group_chat)
+                    st.session_state.group_chat[group_name]['user'] = [Message('test user',1)]
                     temp = []
                     temp.append(group_name)
                     new_contact = temp
                     st.sidebar.radio("", new_contact)
+                    print( st.session_state.group_chat)
                     st.experimental_rerun()
                 else:
                     st.error("Please select at least two to create a group chat.")
@@ -139,3 +150,4 @@ class Chat():
 if __name__ == "__main__":
     chat = Chat('168.235.86.101','VC1', 'VC')
     chat.main()
+    count = st_autorefresh(interval=5000, limit=100, key="data_refresh")
