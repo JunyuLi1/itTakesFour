5852da2a5a7c411ad5f1410279323abd1bb03f61
JunyuLi1 <junyul031030@gmail.com>
2024-06-25 02:19:28 -0700

    add home_template

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 from chat import ds_messenger
 
 class User():
-    def __init__(self, ip, ucername, password):
-        self.contactobj = ds_messenger.DirectMessenger(ip,ucername, password)
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.contactobj = ds_messenger.DirectMessenger(ip,username, password)
