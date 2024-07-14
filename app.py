5226169ac118c5dcafc27be3e510cf22fb9f9275
JunyuLi1 <junyul031030@gmail.com>
2024-07-14 21:30:25 +0800

    add userdb

--- app.py
+++ app.py
@@ -31,26 +31,29 @@ def chat():
     # contact parts = {'SuperHammerB': {'SuperHammerB':[],'user:[]'}}
     #TODO: Impoertant !!! store message in local (new server if applicable)
     global user
-    result = user.contactobj.retrieve_all()
-    contacts = {}
-    for item in result:
-        if item.recipient not in contacts:
-            contacts[item.recipient] = []
-            contacts[item.recipient].append(item.message)
-        else:
-            contacts[item.recipient].append(item.message)
-    if request.method == 'POST':
-        print(request.form)
-        person = request.form['chat_friend']
-        if 'send_message' in request.form:
-            message_input = request.form['send_message']
-            user.contactobj.send(message_input,person)
-        if person not in contacts.keys():
-            contacts[person] = []
-        print(contacts)
-        return render_template('chat.html', contacts=contacts.keys(), history=contacts[person],selected_radio=person)
+    if user is None:
+        return render_template('login.html')
     else:
-        return render_template('chat.html', contacts=contacts.keys())
+        result = user.contactobj.retrieve_all()
+        contacts = {}
+        for item in result:
+            if item.recipient not in contacts:
+                contacts[item.recipient] = []
+                contacts[item.recipient].append(item.message)
+            else:
+                contacts[item.recipient].append(item.message)
+        if request.method == 'POST':
+            print(request.form)
+            person = request.form['chat_friend']
+            if 'send_message' in request.form:
+                message_input = request.form['send_message']
+                user.contactobj.send(message_input,person)
+            if person not in contacts.keys():
+                contacts[person] = []
+            print(contacts)
+            return render_template('chat.html', contacts=contacts.keys(), history=contacts[person],selected_radio=person)
+        else:
+            return render_template('chat.html', contacts=contacts.keys())
 
 if __name__ == '__main__':
     #user = User('168.235.86.101','VC1', 'VC') #config user, could change later
